21d38119a0ff088aa8336e183fafa43d8a4ac7f0
Your Name <luc785@hotmail.com>
2014-09-01 00:19:40 -0400

    Form verification. Need to remap data

--- public/js/controller/maincontroller.js
+++ public/js/controller/maincontroller.js
@@ -1,10 +1,22 @@
 app.controller("MainController", function($scope, $http, Sub){
 		$scope.understand = "I now understand how scope works";
 	    
-       $scope.sendCall = function(){         
-            $scope.understand = Sub.post($scope.user);//'/api/submission', $scope.user)
-              //.success(function(data) {
-			//	$scope.understand = data;
-             // });
+       $scope.sendCall = function(isValid){         
+            if (isValid) {
+                $scope.understand = 'NOT RIGHT';
+            }//$scope.understand = Sub.post($scope.user); 
+            else {
+             	$scope.understand = 'is right';    
+             }
         }
+        
+        	// function to submit the form after all validation has occurred			
+		$scope.submitForm = function(isValid) {
+
+			// check to make sure the form is completely valid
+			if (isValid) { 
+				alert('our form is amazing');
+			}
+		};
+
 });
