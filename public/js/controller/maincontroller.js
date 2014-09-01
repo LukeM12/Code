3df54809c4cb2dc526f3c5be93d018da069e5cf0
Your Name <luc785@hotmail.com>
2014-09-01 00:29:11 -0400

    Brought validated data from angular frontend to mongo

--- public/js/controller/maincontroller.js
+++ public/js/controller/maincontroller.js
@@ -1,22 +1,15 @@
 app.controller("MainController", function($scope, $http, Sub){
 		$scope.understand = "I now understand how scope works";
 	    
-       $scope.sendCall = function(isValid){         
-            if (isValid) {
-                $scope.understand = 'NOT RIGHT';
-            }//$scope.understand = Sub.post($scope.user); 
-            else {
-             	$scope.understand = 'is right';    
-             }
-        }
-        
         	// function to submit the form after all validation has occurred			
-		$scope.submitForm = function(isValid) {
+		$scope.sendForm = function(isValid) {
 
 			// check to make sure the form is completely valid
 			if (isValid) { 
 				alert('our form is amazing');
-			}
+                $scope.understand = Sub.post($scope.user);
+			
+            }
 		};
 
 });
