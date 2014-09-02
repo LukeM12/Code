c811f93ccec11a3e84092387a221a194b8a54504
Your Name <luc785@hotmail.com>
2014-09-02 11:11:42 -0400

    Injected the factory HTTP-DB service into the angular .config module

--- public/js/core.js
+++ public/js/core.js
@@ -1,7 +1,7 @@
 var app = angular.module('MainForm', []);
 
 
-//Configure the main page routes
+/* Configure the dynamic content to go info angularjs */
 app.config(function ($routeProvider) {
 	$routeProvider.when('/', 
 		{
@@ -10,7 +10,23 @@ app.config(function ($routeProvider) {
 		}
 	)
 });
-
-app.controller("formController", function($scope, $http){
+/* Configure the Angular Controller for the form, and inject the submission service */
+app.controller("formController", function($scope, $http, Sub){
 		$scope.understand = "cool";
+
+		$scope.sendForm = function(isValid){
+			Sub.post($scope.user);
+		}
 });
+
+/* Configure the service for form application submissions */
+//TODO, expand this for all the kinds of posts of forms you will be receiving (marketing, PM etc)
+app.factory('Sub', function($http){
+        return {
+            post : function(applicantData) {
+            	return $http.post('/api/submission', applicantData);
+            }
+        }
+});
+
+
