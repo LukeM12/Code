05f4de46f71169c4b7d2ec923ba248173bc1d74e
Your Name <luc785@hotmail.com>
2014-09-02 10:56:19 -0400

    Functionally got a controller with a local routes to the angular subcontainer

--- public/js/core.js
+++ public/js/core.js
@@ -1,1 +1,16 @@
 var app = angular.module('MainForm', []);
+
+
+//Configure the main page routes
+app.config(function ($routeProvider) {
+	$routeProvider.when('/', 
+		{
+			templateUrl:'js/formObject/form.html',
+			controller:'formController'
+		}
+	)
+});
+
+app.controller("formController", function($scope, $http){
+		$scope.understand = "cool";
+});
