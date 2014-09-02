2d8e56e2932b04400f5329fb476d6cf9a6deae16
Your Name <luc785@hotmail.com>
2014-09-02 02:06:39 -0400

    Cleaed up index.html

---
+++ public/js/formObject/formController.js
@@ -0,0 +1,10 @@
+app.controller("formController", function($scope, $http, ){
+		$scope.understand = "cool";
+	    
+        	// function to submit the form after all validation has occurred			
+		$scope.sendForm = function(isValid) {
+            $scope.getit = "hello world";
+        }
+
+});
+
