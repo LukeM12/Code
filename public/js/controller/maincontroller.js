2930e24c731920733c59691ac6b26cbedaa1793e
Your Name <luc785@hotmail.com>
2014-09-02 02:22:07 -0400

    Separated the form logic into an ng-view

--- public/js/controller/maincontroller.js
+++ public/js/controller/maincontroller.js
@@ -1,15 +1,19 @@
-app.controller("MainController", function($scope, $http, Sub){
-		$scope.understand = "I now understand how scope works";
-	    
-        	// function to submit the form after all validation has occurred			
-		$scope.sendForm = function(isValid) {
 
-			// check to make sure the form is completely valid
-			if (isValid) { 
-				alert('our form is amazing');
-                $scope.understand = Sub.post($scope.user);
-			
-            }
-		};
+app.config(function($routeProvider){
+    $routeProvider.when('/', 
+        {
+            templateUrl:"js/controller/form.html",
+            controller:"MainController"
+        }
+    )
+});
+
+app.controller("MainController", function($scope, $http){// Sub){
+			//if (isValid) { 
+			//	alert('our form is amazing');
+              //  $scope.understand = Sub.post($scope.user);
+		//	
+         //   }
+		//};
 
 });
